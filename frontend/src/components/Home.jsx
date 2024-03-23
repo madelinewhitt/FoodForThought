19ab08637f1e91f05cf005b1e4b8c34dedc0da01
madelinewhitt <madelinewhitton@MacBook-Air-10.local>
2024-03-23 19:03:30 -0400

    Frontend Title Page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Home() {
     return (
-        <section className="h-screen pt-16">
+        <section className="bg-welcome_background bg-blend-overlay bg-white bg-opacity-40 h-screen flex flex-col bg-center bg-cover justify-center items-center">
             <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">We Have Food at Home</h1>
-            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">Reimagining Home Cooking: Where Comfort Meets Culinary Excellence</p>
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 ">Reimagining Home Cooking: Where Comfort Meets Culinary Excellence</p>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Learn more
                 <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
